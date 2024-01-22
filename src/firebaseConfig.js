730995a4e3b20af17bba539f7320dcdafedf8c50
Mikail <abdulraheemmukaila6@gmail.com>
2024-01-22 14:39:22 +0100

    modify

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDc2sXI3sw_Lr6vpOIpVjYP9zNeY_wR2mE",
+  authDomain: "darun-nur-production.firebaseapp.com",
+  databaseURL: "https://darun-nur-production-default-rtdb.firebaseio.com",
+  projectId: "darun-nur-production",
+  storageBucket: "darun-nur-production.appspot.com",
+  messagingSenderId: "149484395313",
+  appId: "1:149484395313:web:274001d9bbaecd56c56e10",
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
